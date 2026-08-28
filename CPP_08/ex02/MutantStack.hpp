23293e0cc03252557b2526a150e51acdfbc874cf
NikitaKuydin08 <nikitakuydin@gmail.com>
2026-08-29 02:27:37 +0700

    All Done

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -19,7 +19,6 @@
 
 #include <stack>
 #include <deque>
-// #include <iterator>
 
 // template <class T, class Container=std::deque<T> >
 // class MutantStack : public std::stack<T, Container> {
@@ -33,7 +32,7 @@
 //         ~MutantStack<T, Container>(void) {};
 
 //         typedef typename std::stack<T, Container>::container_type::iterator iterator;
-//         typename std::stack<T, Container>::container_type::const_iterator const_iterator;
+//         typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
 //         typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
 
 //         iterator begin(void) { return (this->c.begin()); }
@@ -42,17 +41,43 @@
 //         reverse_iterator rend(void) { return (this->c.rend()); }
 // };
 
+template <class T>
+class MutantStack : public std::stack<T> {
+    public:
+        MutantStack<T>(void) : std::stack<T>() {};
+        MutantStack<T>(const MutantStack<T>& copy) : std::stack<T>(copy) {};
+        MutantStack<T>& operator=(const MutantStack<T>& copy) {
+            if (this != copy) { this->c = copy.c; }
+            return (*this);
+        }
+        ~MutantStack<T>(void) {};
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+
+        iterator begin(void) { return (this->c.begin()); }
+        iterator end(void) { return (this->c.end()); }
+        reverse_iterator rbegin(void) { return (this->c.rbegin()); }
+        reverse_iterator rend(void) { return (this->c.rend()); }
+        const_iterator begin(void) const { return (this->c.begin()); }
+        const_iterator end(void) const { return (this->c.end()); }
+};
+
 
 template <typename T>
-class MutantStack {
+class CompositionStack {
     protected:
         std::deque<T> deque;
 
     public:
-        MutantStack(void);
-        MutantStack(const MutantStack& copy);
-        MutantStack& operator=(const MutantStack& copy);
-        ~MutantStack(void);
+        CompositionStack(void) {}
+        CompositionStack(const CompositionStack& copy) : deque(copy.getDeque()) {}
+        CompositionStack& operator=(const CompositionStack& copy) {
+            if (this != &copy) { deque = copy.deque; }
+            return (*this);
+        }
+        ~CompositionStack(void) {}
 
         typedef typename std::deque<T>::size_type size_type;
         typedef typename std::deque<T>::value_type value_type;
@@ -83,7 +108,5 @@ class MutantStack {
         }
         const std::deque<T>& getDeque(void) const { return (this->deque); }
 };
-
-#include "MutantStack.tpp"
 
 #endif
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -14,7 +14,7 @@ void print(const Container& c) {
 
 int main(void)
 {
-	std::cout << PURPLE << "MutantStack implementation of a std::stack" << RESET << std::endl;
+	std::cout << PURPLE << "MutantStack inheritance of std::stack" << RESET << std::endl;
     MutantStack<int> mstack;
 	std::cout << YELLOW << "Stack.empty() : " << mstack.empty() << RESET << std::endl;
     mstack.push(42);
@@ -49,136 +49,24 @@ int main(void)
 	std::cout << YELLOW << "List.size() : " << mlist.size() << RESET << std::endl;
 	std::cout << YELLOW << "List.back() : " << mlist.back() << RESET << std::endl;
 
+    std::cout << PURPLE << "MutantStack(CompositionStack) composition \"has a\" relationship. \
+    Direct implementation of std::stack" << RESET << std::endl;
+    CompositionStack<int> cstack;
+	std::cout << YELLOW << "Stack.empty() : " << cstack.empty() << RESET << std::endl;
+    cstack.push(42);
+	std::cout << YELLOW << "Stack.empty() : " << cstack.empty() << RESET << std::endl;
+	cstack.push(0);
+	cstack.push(99);
+	cstack.push(17);
+	cstack.push(13);
+	print(cstack);
+	cstack.pop();
+	print(cstack);
+	cstack.push(50);
+	print(cstack);
+	std::cout << YELLOW << "Stack.size() : " << cstack.size() << RESET << std::endl;
+	std::cout << YELLOW << "List.back() : " << cstack.top() << RESET << std::endl;
+    std::stack<int> sc(cstack);
 
     return 0;
 }
-
-// #include "MutantStack.hpp"
-// #include <list>
-// #include <vector>
-
-// template <typename T>
-// void printElements(const T& container) {
-// 	typename T::const_iterator it = container.begin();
-// 	typename T::const_iterator ite = container.end();
-
-// 	std::cout << "Container : ";
-// 	while (it != ite) {
-// 		std::cout << *it << " ";
-// 		++it;
-// 	}
-// 	std::cout << std::endl;
-// }
-
-// int main() {
-// 	MutantStack<int> stack;
-
-// 	std::cout << PURPLE << "### Testing Stack" << RESET << std::endl;
-// 	std::cout << YELLOW << "stack.empty() : " << stack.empty() << RESET << std::endl;
-// 	stack.push(42);
-// 	std::cout << YELLOW << "stack.empty() : " << stack.empty() << RESET << std::endl;
-
-// 	stack.push(0);
-// 	stack.push(99);
-// 	stack.push(17);
-// 	stack.push(13);
-// 	printElements(stack);
-// 	stack.pop();
-// 	printElements(stack);
-// 	stack.push(50);
-// 	printElements(stack);
-
-// 	std::cout << YELLOW << "stack.size() : " << stack.size() << RESET << std::endl;
-// 	std::cout << YELLOW << "stack.top() : " << stack.top() << RESET << std::endl;
-
-// 	// ##############################################################################
-// 	MutantStack<int>::iterator it = stack.begin();
-// 	MutantStack<int>::iterator ite = stack.end();
-
-// 	std::cout << "Forward iterator : ";
-// 	while (it != ite) {
-// 		std::cout << *it << " ";
-// 		++it;
-// 	}
-// 	std::cout << std::endl;
-
-// 	// ##############################################################################
-// 	MutantStack<int>::reverse_iterator rit = stack.rbegin();
-// 	MutantStack<int>::reverse_iterator rite = stack.rend();
-// 	std::cout << "Reverse iterator : ";
-// 	while (rit != rite) {
-// 		std::cout << *rit << " ";
-// 		++rit;
-// 	}
-// 	std::cout << std::endl;
-
-// 	// ##############################################################################
-// 	const MutantStack<int> constStack = stack;
-
-// 	MutantStack<int>::const_iterator cit = constStack.begin();
-// 	MutantStack<int>::const_iterator cite = constStack.end();
-// 	std::cout << "Const iterator : ";
-// 	while (cit != cite) {
-// 		std::cout << *cit << " ";
-// 		++cit;
-// 	}
-// 	std::cout << std::endl;
-
-// 	// ##############################################################################
-// 	MutantStack<int>::const_reverse_iterator crit = constStack.rbegin();
-// 	MutantStack<int>::const_reverse_iterator crite = constStack.rend();
-// 	std::cout << "Const iterator : ";
-// 	while (crit != crite) {
-// 		std::cout << *crit << " ";
-// 		++crit;
-// 	}
-// 	std::cout << std::endl;
-
-// 	// ##############################################################################
-// 	// ##############################################################################
-
-// 	std::list<int> list;
-
-// 	std::cout << PURPLE << "\n### Testing List" << RESET << std::endl;
-// 	std::cout << YELLOW << "List.empty() : " << list.empty() << RESET << std::endl;
-// 	list.push_back(42);
-// 	std::cout << YELLOW << "list.empty() : " << list.empty() << RESET << std::endl;
-
-// 	list.push_back(0);
-// 	list.push_back(99);
-// 	list.push_back(17);
-// 	list.push_back(13);
-// 	printElements(list);
-// 	list.pop_back();
-// 	printElements(list);
-// 	list.push_back(50);
-// 	printElements(list);
-
-// 	std::cout << YELLOW << "List.size() : " << list.size() << RESET << std::endl;
-// 	std::cout << YELLOW << "List.back() : " << list.back() << RESET << std::endl;
-
-// 	// ##############################################################################
-
-// 	std::list<int>::iterator lit = list.begin();
-// 	std::list<int>::iterator lite = list.end();
-
-// 	std::cout << "Forward iterator : ";
-// 	while (lit != lite) {
-// 		std::cout << *lit << " ";
-// 		++lit;
-// 	}
-// 	std::cout << std::endl;
-
-// 	// ##############################################################################
-
-// 	std::list<int>::reverse_iterator lrit = list.rbegin();
-// 	std::list<int>::reverse_iterator lrite = list.rend();
-
-// 	std::cout << "Reverse iterator : ";
-// 	while (lrit != lrite) {
-// 		std::cout << *lrit << " ";
-// 		++lrit;
-// 	}
-// 	std::cout << std::endl;
-
-
