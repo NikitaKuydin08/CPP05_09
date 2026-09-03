4f5697f54a7ace6c263f7db07a5b945862770639
NikitaKuydin08 <nikitakuydin@gmail.com>
2026-09-04 00:53:48 +0700

    Need to add new function parseDate, to change from std::string to time_t, to be able to compare it with other dates, to find the closest date to the target date

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -16,20 +16,31 @@
 #include <sstream>
 #include <ctime>
 
+// int compare (size_t pos, size_t len, const string& str) - compare(10, 3, " | ")
+
+bool global_valid = true;
+std::map<std::string, float> _data;
+
 // printing function to print input file errors 
-void printError(const char* message) {
+void printError(std::string message) {
     std::cout << "Error: " << message << std::endl;
 }
 
-std::map<std::string, float> _data;
+void printGoodOutput(std::string& date, float& fvalue) {
+    (void)date;
+    (void)fvalue;
+    std::cout << std::endl;
+}
 
-bool check_value_format(std::string value, float& res) {
+bool check_value_format(std::string value, float& res, bool isInput) {
     char *end_ptr;
 
     double temp_res = std::strtod(value.c_str(), &end_ptr);
     if (*end_ptr != '\0' || temp_res < 0)
         return (false);
     res = static_cast<float>(temp_res);
+    if (isInput && (res <= 0 || res > 1000))
+        return (false);
     return (true);
 }
 
@@ -70,7 +81,7 @@ bool make_pairs(std::string line, std::string& exception) {
     }
     float fvalue;
     std::string value = line.substr(11);
-    if (!check_value_format(value, fvalue)) {
+    if (!check_value_format(value, fvalue, false)) {
         exception = "Database's value format is invalid => " + value;
         return (false);
     }
@@ -99,11 +110,74 @@ void BitcoinExchange::fill_map(void) {
     ReadDB.close();
 }
 
+void find_t_closest_date(std::string& date) {
+    std::map<std::string, float>::iterator low, prev;
+    low = _data.lower_bound(date);
+    if (low == _data.end())
+        std::cout << "When do we hit it?" << std::endl;
+    else if (low == _data.begin()) {
+        date = low->first;
+        return ;
+    }
+    else {
+        prev = std::prev(low);
+
+    }
+}
+
+void execute_main_loop(std::string line) {
+    if (line.compare(10, 3, " | ") != 0 || (!isdigit(line[13]) && line[13] != '-'))
+        return (printError("invalid format of line => " + line));
+    std::string date = line.substr(0, 10);
+    if (!check_date_format(date))
+        return (printError("invalid format of date => " + date));
+    float fvalue;
+    std::string value = line.substr(13);
+    if (!check_value_format(value, fvalue, true))
+        return (printError("the value is out of bound => " + value));
+
+    std::map<std::string, float>::iterator it = _data.find(date);
+    if (it == _data.end())
+        find_t_closest_date(date);
+    printGoodOutput(date, fvalue);
+}
+
 // check input file and do multiplication
 void BitcoinExchange::exchange(char* file) {
     std::fstream ReadFile(file, std::ios::in);
     if (!ReadFile.is_open())
         throw(NotOpen());
-    
+    std::string line;
+    if (!std::getline(ReadFile, line) || line.compare("date | value"))
+        throw(std::runtime_error("Input file is emty or of invalid format"));
+    while (std::getline(ReadFile, line)) {
+        execute_main_loop(line);
+    }
     ReadFile.close();
 }
+
+/*
+    date | value
+    2011-01-03 | 3 // passed the check, did multiplication, printed the line
+    2011-01-03 | 2
+    2011-01-03 | 1
+    2011-01-03 | 1.2
+    2011-01-09 | 1
+    2012-01-11 | -1 // failed check, printed the error line
+    2001-42-42
+    2012-01-11 | 1
+    2012-01-11 | 2147483648
+*/
+
+/*
+    $> ./btc input.txt
+    2011-01-03 => 3 = 0.9
+    2011-01-03 => 2 = 0.6
+    2011-01-03 => 1 = 0.3
+    2011-01-03 => 1.2 = 0.36
+    2011-01-09 => 1 = 0.32
+    Error: not a positive number.
+    Error: bad input => 2001-42-42
+    2012-01-11 => 1 = 7.1
+    Error: too large a number.
+*/
